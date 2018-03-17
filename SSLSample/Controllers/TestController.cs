eaac9631840ecdfcc007a05f1d150ca688866259
doylestowncoder <schmidt.chris.j@gmail.com>
2018-03-17 16:28:44 -0400

    Initial Commit for SSL Sample Project

---
+++ SSLSample/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Http;
+
+namespace SSLSample.Controllers
+{
+    public class TestController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            var testResult = new
+            {
+                ApiName = "SSLSample",
+                ServerInstance = Environment.MachineName,
+                Version = "1.0",
+                UsingSSL = (this.ActionContext.Request.RequestUri.Scheme == Uri.UriSchemeHttps),
+                HealthMeter = 100
+            };
+
+            return Ok(testResult);  // Returns an OkNegotiatedContentResult
+        }
+    }
+}
---
+++ SSLSample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SSLSample.WebApiApplication" Language="C#" %>
